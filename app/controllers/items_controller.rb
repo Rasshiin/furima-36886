f3885c5e502be2f025792dd494b2cc1d94d4b995
Rasshiin <89972186+Rasshiin@users.noreply.github.com>
2021-09-28 21:25:51 +0900

    ユーザー登録機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit()
+  end
 end
