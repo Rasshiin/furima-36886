dc1f40162a5118578aa308050544ee671442cb4e
Rasshiin <89972186+Rasshiin@users.noreply.github.com>
2021-09-29 17:05:12 +0900

    ログイン機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,5 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def create
-    @user = User.new(user_params)
-  end
-
-  private
-  def user_params
-    params.require(:user).permit()
-  end
+ 
 end
