721520b263ae9a810e57a467bee255113220451e
Rasshiin <89972186+Rasshiin@users.noreply.github.com>
2021-10-01 16:32:37 +0900

    トップページへ

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    title              {Faker::Internet.name}
+    price              {1000}
+    explanation        {"うまい"}
+    category_id        {3}
+    postage_load_id    {4}
+    items_condition_id {3}
+    prefecture_id      {36}
+    shipment_day_id    {2}
   end
 end
