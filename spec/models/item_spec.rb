a11ffb5756e6fe9bcab478bfbe8b46046566289c
Rasshiin <89972186+Rasshiin@users.noreply.github.com>
2021-10-01 18:34:39 +0900

    テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,12 +37,12 @@ RSpec.describe Item, type: :model do
       it '価格が300円未満であれば出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '価格が9999999円を超えているなら出品できない' do
         @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'カテゴリーが選択されていなければ出品できない' do
         @item.category_id = 1
@@ -65,14 +65,14 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数が選択されていなければ出品できない' do
-        expect.shipment_day_id = 1
+        @item.shipment_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("")
+        expect(@item.errors.full_messages).to include("Shipment day can't be blank")
       end
       it '画像がなければ出品できない' do
-        @item.image = ""
+        @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'ユーザーが紐付いていなければ出品できない' do
         @item.user = nil
