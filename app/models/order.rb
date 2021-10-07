383c45ffca65fcca2a5d0235cf8ffafae73f7e32
Rasshiin <89972186+Rasshiin@users.noreply.github.com>
2021-10-07 17:57:11 +0900

    with_optionsの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,16 @@ class Order
   include ActiveModel::Model
   attr_accessor :postal_code, :municipalities, :address, :building_name, :telephone, :prefecture_id, :purchase_history_id, :user_id, :item_id, :token
 
-  validates :postal_code, presence: true, format:{with:/\A\d{3}[-]\d{4}\z/}
-  validates :municipalities, presence: true
-  validates :telephone, presence: true, format:{with:/\A\d[0-9]{9,10}\z/}
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :address, presence: true
-  validates :token, presence: true
-  validates :user_id, presence: true
-  validates :item_id, presence: true
+  with_options presence: true
+    validates :postal_code, format:{with:/\A\d{3}[-]\d{4}\z/}
+    validates :municipalities
+    validates :telephone, format:{with:/\A\d[0-9]{9,10}\z/}
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :address
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
 
   def save
     purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id)
